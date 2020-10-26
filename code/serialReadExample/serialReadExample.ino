80adb96b71b027a6752db154a04ac0239f1c6c5f
Clement Zheng <clement.zheng@gmail.com>
2020-10-26 20:28:16 +0800

    update

--- code/serialReadExample/serialReadExample.ino
+++ code/serialReadExample/serialReadExample.ino
@@ -1,11 +1,8 @@
 // Read data from the serial and turn ON or OFF a light depending on the value
 
 char val; // Data received from the serial port
-int ledPin = 13; // Set the pin to digital I/O 4
-
 
 void setup() {
-  pinMode(ledPin, OUTPUT); // Set pin as OUTPUT
   Serial.begin(9600); // Start serial communication at 9600 bps
 }
 
